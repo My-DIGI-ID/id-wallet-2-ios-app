5ab9459bd1efdf17d40aabf01a96a9d618cdc51a
Moritz Hader <moritz.hader@de.ibm.com>
2021-11-22 14:08:59 +0100

    Initial Commit

---
+++ Sources/IDWallet/IDWalletApp.swift
@@ -0,0 +1,17 @@
+//
+//  IDWalletApp.swift
+//  IDWallet
+//
+//  Created by Moritz Hader on 22.11.21.
+//
+
+import SwiftUI
+
+@main
+struct IDWalletApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
