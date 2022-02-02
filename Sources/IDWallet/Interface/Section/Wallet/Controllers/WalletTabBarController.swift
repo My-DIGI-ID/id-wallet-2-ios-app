0c6462f9bc8e07b37ef0987cdb00c6baaa8dcc3f
Moritz Hader <moritz.hader@de.ibm.com>
2022-02-02 12:44:26 +0100

    Fixed yet another image-name problem

--- Sources/IDWallet/Interface/Section/Wallet/Controllers/WalletTabBarController.swift
+++ Sources/IDWallet/Interface/Section/Wallet/Controllers/WalletTabBarController.swift
@@ -76,7 +76,7 @@ class WalletTabBarController: BareBaseViewController {
     private var qrcodeScanController = UIViewController()
     private var qrcodeScanBarItem = UITabBarItem(
         title: "QR-Code Scan",
-        image: UIImage.requiredImage(name: "BarbuttonQrcodeBig").withRenderingMode(.alwaysOriginal),
+        image: UIImage.requiredImage(name: "BarButtonQrcodeBig").withRenderingMode(.alwaysOriginal),
         selectedImage: UIImage.requiredImage(name: "BarbuttonQrcodeBigSelected").withRenderingMode(.alwaysOriginal))
 
     private var activitiesController = UIViewController()
