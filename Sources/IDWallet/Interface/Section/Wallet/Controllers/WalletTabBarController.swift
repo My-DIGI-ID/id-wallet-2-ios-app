4662e81f95cfb139405d707f80d3cd6ab4215384
Michael Utech <michael@utech.de>
2022-02-08 09:17:30 +0100

    BKAWALIOS-177: Create top level presenter
    Squashed commit of the following:
    
    commit e1d1b812cf512590606c93fea2580d3419a5b58e
    Author: Michael Utech <michael@utech.de>
    Date:   Mon Feb 7 11:48:24 2022 +0100
    
        BKAWALIOS-177: Create top level presenter
    
    commit 598fe5bd74cad7d7a2f646f697d75898b782e4b5
    Author: Andreas Kostuch <andreas@kostuch.at>
    Date:   Fri Feb 4 15:33:33 2022 +0100
    
        BKAWALIOS-114 Add localization
    
    commit 3e36de941fe056deb1bd6f8a24b351855cbf0d1d
    Author: Andreas Kostuch <andreas@kostuch.at>
    Date:   Thu Feb 3 20:17:17 2022 +0100
    
        BKAWALIOS-114 Add String Localize extension
    
    commit 17c18724063f08b34200702498fc03b98015b6c2
    Author: Michael Utech <michael@utech.de>
    Date:   Mon Feb 7 11:46:49 2022 +0100
    
        BKAWALIOS-114 Add QR-Scanner
    
    commit c88d4dcec2763857ca5ae4a3f7f36b6c05e5e7ef
    Author: Michael Utech <michael@utech.de>
    Date:   Mon Feb 7 08:46:11 2022 +0100
    
        BKAWALIOS-177: Create top level presenter

--- Sources/IDWallet/Interface/Section/Wallet/Controllers/WalletTabBarController.swift
+++ Sources/IDWallet/Interface/Section/Wallet/Controllers/WalletTabBarController.swift
@@ -79,7 +79,7 @@ class WalletTabBarController: BareBaseViewController {
     private var qrcodeScanController = UIViewController()
     private var qrcodeScanBarItem = UITabBarItem(
         title: "QR-Code Scan",
-        image: UIImage.requiredImage(name: "BarButtonQrcodeBig").withRenderingMode(.alwaysOriginal),
+        image: UIImage.requiredImage(name: "BarbuttonQrcodeBig").withRenderingMode(.alwaysOriginal),
         selectedImage: UIImage.requiredImage(name: "BarbuttonQrcodeBigSelected").withRenderingMode(.alwaysOriginal))
 
     private var activitiesController = UIViewController()
