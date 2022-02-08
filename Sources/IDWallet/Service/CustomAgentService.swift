a42f89cd8f4e48795cb61f5a8d837c8fd9034fa6
Marco Seidel <61539446+Envidual-Marco@users.noreply.github.com>
2022-02-08 13:43:05 +0100

    Created services to interact with Aries

---
+++ Sources/IDWallet/Service/CustomAgentService.swift
@@ -0,0 +1,35 @@
+//
+// Copyright 2022 Bundesrepublik Deutschland
+//
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+// the License. You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+// an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+// specific language governing permissions and limitations under the License.
+//
+
+import Foundation
+import Aries
+
+class CustomAgentService {
+    private static let id = "ID"
+    private static let key = "KEY"
+    private static let genesis = "idw_eesditest"
+    
+    func setup() async throws {
+        guard let url = Bundle.main.url(forResource: Self.genesis, withExtension: nil)?.absoluteString else {
+            return
+        }
+        
+        // First time setup of the agent
+        try await Aries.agent.setup(with: Self.id, Self.key, url)
+        
+        // Set the first master secret to enable credential handling
+        try await Aries.agent.run {
+            try await Aries.provisioning.update(Self.id, with: $0)
+        }
+    }
+}
---
+++ Sources/IDWallet/Service/CustomCredentialService.swift
@@ -0,0 +1,78 @@
+//
+// Copyright 2022 Bundesrepublik Deutschland
+//
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+// the License. You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+// an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+// specific language governing permissions and limitations under the License.
+//
+
+import Foundation
+import Aries
+
+class CustomCredentialService {
+    func credentials() async throws -> [CredentialRecord] {
+        try await Aries.agent.run {
+            try await Aries.record.search(CredentialRecord.self, in: $0.wallet, with: .none, count: nil, skip: nil)
+        }
+    }
+    
+    func request(with connectionId: String) async throws -> String {
+        try await Aries.agent.run {
+            let connection = try await Aries.record.get(ConnectionRecord.self, for: connectionId, from: $0.wallet)
+            
+            guard let service = connection.theirDocument().services?.first else {
+                return ""
+            }
+            
+            var credentialPreview = CredentialPreview()
+            credentialPreview.attributes.append(contentsOf: [
+                CredentialAttribute(name: "firstName", value: "Erika"),
+                CredentialAttribute(name: "lastName", value: "Mustermann"),
+                CredentialAttribute(name: "firmName", value: "MESA Deutschland GmbH"),
+                CredentialAttribute(name: "firmSubject", value: "Identitäten"),
+                CredentialAttribute(name: "firmCity", value: "Berlin"),
+                CredentialAttribute(name: "firmPostalcode", value: "51145"),
+                CredentialAttribute(name: "firmStreet", value: "Musterstrasse 2")
+            ])
+            
+            var credentialProposal: CredentialProposalMessage = try await Aries.credential.proposal()
+            credentialProposal.credentialId = "akaikNikeYARE9DnS9Jox:3:CL:23:Arbeitgeberbescheinigung_Test3"
+            credentialProposal.proposal = credentialPreview
+            
+            let credentialProposalRequest = MessageRequest(
+                message: credentialProposal,
+                recipientKeys: service.recipientKeys ?? [],
+                senderKey: connection.myVerkey,
+                endpoint: service.endpoint
+            )
+            
+            let credentialProposalResponse: MessageResponse<CredentialOfferMessage> = try await Aries.message
+                .sendReceive(credentialProposalRequest, with: $0.wallet)
+            
+            let credentialOffer = credentialProposalResponse.message
+            
+            let id = try await Aries.credential.process(credentialOffer, for: connectionId, with: $0)
+            
+            let credentialRequest = try await Aries.credential.request(for: id, with: $0)
+            
+            let credentialRequestRequest = MessageRequest(
+                message: credentialRequest,
+                recipientKeys: service.recipientKeys ?? [],
+                senderKey: connection.myVerkey,
+                endpoint: service.endpoint
+            )
+            
+            let credentialRequestResponse: MessageResponse<CredentialIssueMessage> = try await Aries.message
+                .sendReceive(credentialRequestRequest, with: $0.wallet)
+            
+            let credentialIssue = credentialRequestResponse.message
+            
+            return try await Aries.credential.process(credentialIssue, with: $0)
+        }
+    }
+}
